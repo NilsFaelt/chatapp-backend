dce5563af23c06013baf306c9b95ee1e3cee5932
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-06-16 22:37:13 +0200

    first upload

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const port = 4001;
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const server = http.createServer(app);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User with id ${socket.id} connected`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(data);
+  });
+
+  socket.on("send_message", (data) => {
+    console.log(data.message);
+  });
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("recive_message", data);
+    console.log("sneding a sms", data);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
