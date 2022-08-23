133e13650f0c5f341e6e278651939486b25d5afe
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-08-23 16:22:02 +0200

    PORT

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT;
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
