d5e764f060665708622585e2062165cec2195aaf
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-08-23 22:25:20 +0200

    uo

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: ["https://chatt-app-client.herokuapp.com"],
     methods: ["GET", "POST"],
     credentials: true,
   },
