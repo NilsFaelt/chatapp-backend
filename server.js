8f540cfaddef9510612e998290caaa598657190d
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-08-24 13:49:40 +0200

    add

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const io = new Server(port, {
+const io = new Server(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
