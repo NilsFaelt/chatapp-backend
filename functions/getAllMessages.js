ef0ae4efcad4c57e13dab850ae3b5349e6879110
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-08-24 14:19:42 +0200

    ro

--- functions/getAllMessages.js
+++ functions/getAllMessages.js
@@ -3,7 +3,7 @@ const { db } = require("../db/db");
 async function getAllMessages(data) {
   const sql = `SELECT * FROM messages WHERE room = $1`;
   const result = await db.query(sql, [data.room], function (error, results) {
-    return result.rows;
+    return result;
   });
 }
 
