981e1ca55edf14080ac4b4f1cb742df3dfa23bb6
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-08-24 14:36:40 +0200

    sms

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const { Client } = require("pg");
 
 const createMessages = `CREATE TABLE IF NOT EXISTS messages(message TEXT, room TEXT, user TEXT, date TEXT)`;
 const createChatRooms = `CREATE TABLE IF NOT EXISTS chatRooms(name TEXT)`;
-const testRoom = `CREATE TABLE IF NOT EXISTS mess(message TEXT, room TEXT)`;
+const testRoom = `CREATE TABLE IF NOT EXISTS sms(message TEXT, room TEXT, user TEXT, date TEXT)`;
 
 const db = new Client({
   ssl: {
